49e1e20fade8c86273686ad39bdff9fefa07272b
SHIRO-STUDIOX <shiroishida185@gmail.com>
2024-10-06 14:19:53 +0330

    change names Comment

--- Setup.sh
+++ Setup.sh
@@ -1,15 +1,15 @@
-# متغیرها
-HUGGING_FACE_TOKEN="hf_your_token_here"  # توکن Hugging Face خود را وارد کنید
-UNET_MODEL_LINK="https://huggingface.co/black-forest-labs/FLUX.1-dev/resolve/main/flux1-dev.safetensors"  # لینک فایل Model UNET
-VAE_MODEL_LINK="https://huggingface.co/black-forest-labs/FLUX.1-dev/resolve/main/ae.safetensors"  # لینک فایل Model VAE
-CLIP_TEXTENCODER_MODEL_LINK="https://huggingface.co/comfyanonymous/flux_text_encoders/resolve/main/clip_l.safetensors"  # لینک فایل CLIP FLUX Text Encoder
-CLIP_FP16_MODEL_LINK="https://huggingface.co/comfyanonymous/flux_text_encoders/resolve/main/t5xxl_fp16.safetensors"  # لینک فایل CLIP FP16
+# Variables
+HUGGING_FACE_TOKEN="hf_your_token_here"  # Hugging Face Token 
+UNET_MODEL_LINK="https://huggingface.co/black-forest-labs/FLUX.1-dev/resolve/main/flux1-dev.safetensors"  # Model UNET
+VAE_MODEL_LINK="https://huggingface.co/black-forest-labs/FLUX.1-dev/resolve/main/ae.safetensors"  # Model VAE
+CLIP_TEXTENCODER_MODEL_LINK="https://huggingface.co/comfyanonymous/flux_text_encoders/resolve/main/clip_l.safetensors"  # CLIP FLUX Text Encoder
+CLIP_FP16_MODEL_LINK="https://huggingface.co/comfyanonymous/flux_text_encoders/resolve/main/t5xxl_fp16.safetensors"  # CLIP FP16
 
-# مسیرهای ذخیره‌سازی
-SAVE_PATH_UNET="/workspace/ComfyUI/models/unet/"  # مسیر ذخیره‌سازی Model UNET
-SAVE_PATH_VAE="/workspace/ComfyUI/models/vae/"  # مسیر ذخیره‌سازی Model VAE
-SAVE_PATH_CLIP_TEXTENCODER="/workspace/ComfyUI/models/clip/"  # مسیر ذخیره‌سازی CLIP FLUX Text Encoder
-SAVE_PATH_CLIP_FP16="/workspace/ComfyUI/models/clip/"  # مسیر ذخیره‌سازی CLIP FP16
+# Save Path File
+SAVE_PATH_UNET="/workspace/ComfyUI/models/unet/"  # Save Path Model UNET
+SAVE_PATH_VAE="/workspace/ComfyUI/models/vae/"  # Save Path Model VAE
+SAVE_PATH_CLIP_TEXTENCODER="/workspace/ComfyUI/models/clip/"  # Save Path CLIP FLUX Text Encoder
+SAVE_PATH_CLIP_FP16="/workspace/ComfyUI/models/clip/"  # Save Path CLIP FP16
 
 # تابع Download و حفظ نام فایل اورجینال
 download_model() {
